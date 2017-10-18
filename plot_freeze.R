6ceb7aeb799d3c78f9fa0de185d94fd46ef5a6ff
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-10-18 13:26:32 +0100

    Fix input and axis

--- plot_freeze.R
+++ plot_freeze.R
@@ -1,10 +1,13 @@
 library("ggplot2")
 
-df <- read.csv("example.csv", header=T)
+df <- read.csv("freeze_timings.csv", header=T)
 g <- ggplot(
       data=df,
       aes(x=num_files, y=time, group=code, colour=code)
 )
 g <- g + geom_line()
+g <- g + geom_point()
+g <- g + ylab("Time (s)")
+g <- g + xlab("Number of 10 byte files")
 
 ggsave("freeze_timings.png")
